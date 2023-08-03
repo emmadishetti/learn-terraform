b67f8a83f87759b504a6af8b4527a18cfcecaf50
suresh <surya2fun@gmail.com>
2023-08-04 00:58:42 +0530

    hello world tf

---
+++ practice-terraform/practice-map2.tf
@@ -0,0 +1,17 @@
+variable "fruits_stock_with_price" {
+  default = {
+    apple = {
+      stock =100
+      price =50
+    }
+    banana = {
+      stock
+      =200
+      price =100
+    }
+    }
+}
+
+output "fruits_stock_with_price_banana" {
+  value= var.fruits_stock_with_price["banana"].price
+}
