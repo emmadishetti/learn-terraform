9c434a492e0d823bed9efdc01703f9fb77964687
suresh <surya2fun@gmail.com>
2023-08-08 22:33:48 +0530

    terraform modules

--- module/local-module/data.tf
+++ module/local-module/data.tf
@@ -1,8 +1,7 @@
 data "aws_ami_ids" "ami" {
-  most_recent = true
   name_regex       = "Centos-*"
   owners           = ["576740149353"]
 }
 #output "ami" {
-  value= data."aws_ami"."ami"
-}#
+  value= data.aws_ami_ids.ami
+} #
