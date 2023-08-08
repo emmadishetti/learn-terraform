4950cb3c38c4d48ae3ad1c3817118841fba3b752
suresh <surya2fun@gmail.com>
2023-08-08 23:38:52 +0530

    terraform modules

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "instance" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
 
   tags = {
     Name = "HelloWorld"
