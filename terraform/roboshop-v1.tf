9df667ceab6c9ed48dfe7dc2677fdbb92a4a069e
suresh <surya2fun@gmail.com>
2023-08-09 11:24:21 +0530

    roboshop instances

---
+++ terraform/roboshop-v1.tf
@@ -0,0 +1,178 @@
+resource "aws_instance" "frontend" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "frontend-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+resource "aws_instance" "mongodb" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "mongodb-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+resource "aws_instance" "catalogue" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "catalogue-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+resource "aws_instance" "redis" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "redis"
+  }
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "redis-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "user" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "user"
+  }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "user-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
+resource "aws_instance" "mysql" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "mysql"
+  }
+}
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "mysql-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+resource "aws_instance" "shipping" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "shipping"
+  }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "shipping-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "rabbitmq-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
+resource "aws_instance" "payment" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "payment"
+  }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "payment-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
+}
+
+resource "aws_instance" "cart" {
+  ami           = "ami-03265a0778a880afb"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [ "sg-0f56c7274c7d31689" ]
+
+  tags = {
+    Name = "cart"
+  }
+}
+resource "aws_route53_record" "cart" {
+  zone_id = "Z009544921RTWVTZ5968R"
+  name    = "cart-dev.devops7874.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
