274e4da2b82ce6bb074d9b6be8dd13ee70494f9a
suresh <surya2fun@gmail.com>
2023-08-09 12:46:12 +0530

    roboshop instances

---
+++ mai.tf
@@ -0,0 +1,9 @@
+data "aws_ami" "ami" {
+  executable_users = ["amazon"]
+  most_recent      = true
+  name_regex       = "centos-*"
+  owners           = ["576740149353"]
+}
+  output "ami" {
+    value = data.aws_ami.ami
+  }
