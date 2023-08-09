fbe0ee83ecf40230accd18fcbaf1cf69dcbab81e
suresh <surya2fun@gmail.com>
2023-08-09 12:48:42 +0530

    roboshop instances

--- data sources/main.tf
+++ data sources/main.tf
@@ -4,6 +4,6 @@ data "aws_ami" "ami" {
   name_regex       = "centos-*"
   owners           = ["576740149353"]
 }
-  output "ami" {
-    value = data.aws_ami.ami
-  }
+output "ami" {
+  value = data.aws_ami.ami
+}
