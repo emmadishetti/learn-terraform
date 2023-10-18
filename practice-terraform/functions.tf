e82233d888e34642e7218778a56ecf51effbe654
suresh <surya2fun@gmail.com>
2023-10-18 23:22:15 +0530

    terraform module roboshop

--- practice-terraform/functions.tf
+++ practice-terraform/functions.tf
@@ -11,26 +11,6 @@ what?
 
 
 
-module "elasticache" {
-source = "git::https://github.com/emmadishetti/tf-module-elasticache.git"
-tags   = var.tags
-env    = var.env
-
-for_each = var.elasticache
-
-subnet_ids      = local.db_subnets
-vpc_id          = local.vpc_id
-sg_ingress_cidr = local.app_subnets_cidr
-
-elasticache_type       = each.value["elasticache_type"]
-port                   = each.value["port"]
-family                 = each.value["family"]
-engine                 = each.value["engine"]
-engine_version         = each.value["engine_version"]
-num_cache_nodes        = each.value["num_cache_nodes"]
-node_type              = each.value["node_type"]
-
-}
 
 module "rabbitmq" {
 source = "git::https://github.com/emmadishetti/tf-module-rabbitmq.git"
