79d8d31d4c755df6060242d98373c0372b1fabb5
suresh <surya2fun@gmail.com>
2023-10-19 00:14:50 +0530

    terraform module roboshop

--- practice-terraform/functions.tf
+++ practice-terraform/functions.tf
@@ -7,6 +7,21 @@ ay
 what?
 
 
+module "rabbitmq" {
+source           = "git::https://github.com/emmadishetti/tf-module-rabbitmq.git"
+tags             = var.tags
+env              = var.env
+ami              = var.ami
+zone_id          = var.zone_id
+
+for_each = var.rabbitmq
+
+sg_ingress_cidr  = local.app_subnets_cidr
+ssh_ingress_cidr = var.ssh_ingress_cidr
+subnet_ids       = local.db_subnets
+vpc_id           = local.vpc_id
+instance_type    = each.value["instance_type"]
+}
 
 
 
